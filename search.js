00cb17111e20dfbe8ed02d2d96a35c69025092e5
KIMSEOKJIN <102217654+SEOKKAMONI@users.noreply.github.com>
2022-07-08 07:40:00 +0900

    서브 검색창 연동, 오류 수정

--- search.js
+++ search.js
@@ -1,18 +1,33 @@
+// 내일 해야할것 중복검색시 중복되는 UI/UX
+
 // http://ws.audioscrobbler.com/2.0/?method=track.search&track=아이유&api_key=502aca31cb330e8135b04d480caf6a56&format=json
 const mainInput = document.getElementById("__input"); // main input
 const mainForm = document.getElementById("__form"); // main form
 
 const mainPg = document.querySelector(".main_pg");
 const resultPg = document.getElementById("result_page");
-const serachArea = document.querySelector(".search")
+const serachArea = document.querySelector(".search");
+
+const subForm = document.getElementById("sub__form"); // 서브 form
+const subInput = document.getElementById("sub__input"); // 서브 input
 
 
-mainForm.addEventListener("submit", addMusicList);
+mainForm.addEventListener("submit", inputMain);
+subForm.addEventListener("submit", inputSub);
+
+function inputMain(e) {
+  let musicKeyWorld = mainInput.value;
+  addMusicList(e, musicKeyWorld);
+}
+
+function inputSub(e) {
+  let musicKeyWorld = subInput.value;
+  addMusicList(e, musicKeyWorld);
+}
 
 // 앨범, 음악 정보
-function addMusicList(e) {
+function addMusicList(e, musicKeyWorld) {
   e.preventDefault()
-  let musicKeyWorld = mainInput.value;
   $.ajax({
     type: 'GET',
     url: 'http://ws.audioscrobbler.com/2.0/?method=track.search&track=' + musicKeyWorld + '&api_key=502aca31cb330e8135b04d480caf6a56&format=json',
@@ -20,11 +35,23 @@ function addMusicList(e) {
     .done(function (response) {
       artistInformaition(musicKeyWorld);
       mainPg.style.display = "none"; // 검색이 되면 메인페이지를 가리고
-      resultPg.style.display = "block"; // 검색 결과를 보여준다
       serachArea.style.display = "none"; // 검색창을 가려준다
 
       let musicList = response["results"]["trackmatches"]["track"];
 
+      // 검색결과가 없을때
+      if(musicList.length == 0) {
+        let searchResult = document.querySelector(".searchResult"); // 검색결과
+        let errorArea = document.querySelector(".errorArea");
+        errorArea.style.display = "block";
+        searchResult.innerText = musicKeyWorld;
+      } else { // 검색결과가 있을때
+        resultPg.style.display = "block"; // 검색 결과를 보여준다
+        artistName.innerText = musicKeyWorld;
+      }
+      subInput.value = "";
+      mainInput.value = "";
+      
 
       let count = 1;
       for (let i = 0; i < 13; i++) {
@@ -42,7 +69,6 @@ function addMusicList(e) {
         
 
       }
-      mainInput.value = " "; // 검색창 지워주기
     });
 }
 
@@ -65,13 +91,14 @@ function artistInformaition(musicKeyWorld) {
       let artist_name = response["artist"]["name"]; // 이름
       let detail_url = response["artist"]["url"]; // url
 
+      console.log(detail_url, artist_publish)
+
       $("<a>").attr({ // 자세히보기 버튼 생성후 추가
         value: "자세히 보기",
         href: detail_url,
         class: "more_btn",
       }).appendTo($("#detail_area")).text("자세히 보기");
 
-      artistName.innerText = artist_name;
       artistPublished.innerText = artist_publish;
     });
 }
